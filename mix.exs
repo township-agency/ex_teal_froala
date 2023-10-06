f75ffee1798695011d47d7095654d6247b1d1504
Scott Taylor <staylorwr@users.noreply.github.com>
2023-10-06 16:58:46 -0400

    feat: Add CI

--- mix.exs
+++ mix.exs
@@ -21,11 +21,9 @@ defmodule ExTealFroala.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_teal, path: "../ex_teal"},
+      {:ex_teal, "~> 0.27"},
       {:plug, ">= 1.7.0 or >= 2.1.0"},
-      {:jason, ">= 1.0.0"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:jason, ">= 1.0.0"}
     ]
   end
 end
