b3e068e202a71b4a2d5654a57c7920f2730f833b
Scott Taylor <staylorwr@users.noreply.github.com>
2023-10-09 09:33:36 -0400

    chore: clean up the build system, turns out froala is real big

--- assets/build.mjs
+++ assets/build.mjs
@@ -2,17 +2,23 @@ import ESBuild from "esbuild";
 import vuePlugin from "esbuild-vue";
 import chokidar from "chokidar";
 
+const production = process.argv.includes("--deploy");
+const env = production ? "production" : "development";
+
 const ctx = await ESBuild.context({
   entryPoints: [
     "./src/main.js"
+  ],
+  external: [
+    'vue'
   ],
   bundle: true,
   outdir: "../priv/static/",
   plugins: [vuePlugin()],
   define: {
-    "process.env.NODE_ENV": JSON.stringify("development"),
+    "process.env.NODE_ENV": JSON.stringify(env),
   },
-  minify: process.argv.includes("--deploy")
+  minify: production
 })
 
 if (process.argv.includes("--watch")) {
