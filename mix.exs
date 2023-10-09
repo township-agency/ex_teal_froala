3c1a40a83ce8bb8f2404262f90b16055c66c4926
Scott Taylor <staylorwr@users.noreply.github.com>
2023-10-09 09:18:08 -0400

    Setup documentation and package details for a release (#2)

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule ExTealFroala.MixProject do
   use Mix.Project
 
+  @source "https://github.com/township-agency/ex_teal_froala"
+
   def project do
     [
       app: :ex_teal_froala,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "ExTealFroala",
+      source: @source,
+      package: package(),
+      docs: docs(),
+      description: "A Teal wrapper for the Froala WYSIWYG Editor"
     ]
   end
 
@@ -24,7 +31,25 @@ defmodule ExTealFroala.MixProject do
       {:ex_teal, "~> 0.27"},
       {:plug, ">= 1.7.0 or >= 2.1.0"},
       {:jason, ">= 1.0.0"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* CHANGELOG* priv),
+      maintainers: ["Scott Taylor", "Township Agency"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "CHANGELOG.md"],
+      source_url: @source
     ]
   end
 end
