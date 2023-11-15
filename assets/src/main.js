1901b47c763a957e00e508d836599247d21adb38
Scott Taylor <staylorwr@users.noreply.github.com>
2023-11-15 13:41:01 -0500

    fix: Teal overrides default list styles (#6)
    * fix: Teal overrides default list styles
    
    * [s] build

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,7 +1,7 @@
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import 'froala-editor/js/plugins.pkgd.min'; // all plugins (you can add plugins by one too)
-
 import 'froala-editor/css/froala_style.min.css';
+import './app.css';
 import DetailField from './components/DetailField.vue';
 import FormField from './components/FormField.vue';
 import VueFroala from 'vue-froala-wysiwyg';
